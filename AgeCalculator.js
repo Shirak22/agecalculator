7c8f14792b257eef256169df2973c038b3e8e59c
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-09-12 22:19:26 +0200

    add AgeCalculator

---
+++ AgeCalculator.js
@@ -0,0 +1,58 @@
+
+export default class AgeCalculator {
+    constructor(birthdate) {
+        this.data_input = new Date(birthdate);
+        this.currentDate = new Date();
+
+    }
+
+    getYears = () => {
+        const { currentDate, data_input } = this;
+        let age = currentDate.getFullYear() - data_input.getFullYear();
+        let m = currentDate.getMonth() - data_input.getMonth();
+
+        if (m < 0 || m === 0 && currentDate.getDate() < data_input.getDate()) {
+            age--;
+        }
+
+        return age;
+
+    }
+
+    getMonths = () => {
+        let monthDiff = this.currentDate.getMonth() - this.data_input.getMonth();
+        let ageDays = this.currentDate.getDate() - this.data_input.getDate();
+
+        if (ageDays < 0) {
+            monthDiff--;
+        }
+
+        if (monthDiff < 0) {
+            monthDiff += 12;
+        }
+
+        return monthDiff;
+    }
+
+    daysInMonth = (year, month) => {
+        //returns the last day in the selected month and year
+        return new Date(year, month, 0).getDate();
+    }
+    getDays = () => {
+        let ageDays = this.currentDate.getDate() - this.data_input.getDate();
+        let ageMonths = this.currentDate.getMonth() - this.data_input.getMonth();
+        if (ageDays < 0) {
+            ageDays += this.daysInMonth(this.data_input.getFullYear(), this.currentDate.getMonth());
+
+        }
+
+        if (ageMonths < 0) {
+            let daysDiff = this.currentDate.getDate() - this.data_input.getDate() + this.daysInMonth(this.data_input.getFullYear(), this.data_input.getMonth());
+            ageDays = daysDiff;
+        }
+        return ageDays;
+
+    }
+
+
+}
