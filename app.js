56a5ee2187ba716cc64e0d15a794140546227f43
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-09-13 11:18:45 +0200

    Fix the render and validate date

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 import AgeCalculator from "./AgeCalculator.js";
+
+
+
+const outputComponent_html = (years, months, days) => {
+    return `
+                    <h1 data-years><span >${years}</span>years</h1>
+                    <h1 data-months><span>${months}</span>months</h1>
+                    <h1 data-days> <span>${days}</span>days</h1>
+            `;
+}
+
+
+
+const getInput = (type) => {
+    const input = document.querySelector(`${type} > input`).value;
+    return input;
+}
+
+
+
+
 const app = () => {
+    let output = document.querySelector('[data-output]');
     let btn = document.querySelector('.btn_submit');
     btn.addEventListener('click', (e) => {
         e.preventDefault();
-        let getYear = parseInt(document.querySelector('input[name=year]').value);
-        let getMonth = parseInt(document.querySelector('input[name=month]').value);
-        let getDay = parseInt(document.querySelector('input[name = day]').value);
-        let date = `${getYear}-${getMonth <= 9 ? '0' + getMonth : getMonth}-${getDay <= 9 ? '0' + getDay : getDay}`;
+        clearAlert();
+        let birthDate = `${getInput('.SECTION_YEAR')}-${getInput('.SECTION_MONTH')}-${getInput('.SECTION_DAY')}` ; 
+        const calc = new AgeCalculator(birthDate);
+         if(calc.getYears() > 0 && calc.getMonths()!== NaN && calc.getDays !== NaN ){
+            output.innerHTML = outputComponent_html(calc.getYears(), calc.getMonths(), calc.getDays());
+         }else {
+          alert();
+         }
+      });
+    };
+      
 
-        let calc = new AgeCalculator(date);
-
-        document.querySelector('[data-years] > span').textContent = calc.getYears();
-        document.querySelector('[data-months]> span').textContent = calc.getMonths();
-        document.querySelector('[data-days]> span').textContent = calc.getDays();
-    });
-
+function alert(){
+          document.querySelector('.SECTION_YEAR input').style.borderColor = "#ff0000"
+          document.querySelector('.SECTION_MONTH input').style.borderColor = "#ff0000"
+          document.querySelector('.SECTION_DAY input').style.borderColor = "#ff0000"; 
+}
+function clearAlert(){
+          document.querySelector('.SECTION_YEAR input').style.borderColor = "rgba(0,0,0,.2)"
+          document.querySelector('.SECTION_MONTH input').style.borderColor = "rgba(0,0,0,.2)"
+          document.querySelector('.SECTION_DAY input').style.borderColor = "rgba(0,0,0,.2)" 
 }
 app();
